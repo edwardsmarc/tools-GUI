faadbb54c7beae0884d4884bfa2f1336fb45ab9a
Marc Edwards <medwards219@gmail.com>
2017-02-09 17:29:06 -0800

    New function to create mdr based target tables. Based on evalTargets but simplified.

---
+++ functions/gen_target_table.R
@@ -0,0 +1,47 @@
+# Marc Edwards - BEACONS
+# Feb 7 2017
+# New function based on evalTargets_NWB.R to generate target tables based on MDR representation. Proportion of each class in the ecoregion is multiplied by the MDR value to get target.
+# based on evalTargets_NWB, simplified by removing protected area values from tables. Also removes range and type columns that showed raster value descriptors. Users should now refer to their raster value class breaks in a separate table.
+  
+gen_target_table <- function(mdr, ecoRast, ecoBnd, outFile) {
+  
+  library(raster)
+
+  # Checks 
+  ############################
+  # raster values exist
+  if(file.exists(ecoRast)){
+    ecoR <- raster(ecoRast)
+  } else{
+    stop(paste0("File does not exist: ", ecoRast))
+  }
+  
+  # Calculate ecoregion area using eco boundary
+  ecoAsc <- raster(ecoBnd)
+  ecoAsc[ecoAsc == 0] <- NA # If raster has zero values filling the extent, convert them to NA 
+  cellsize <- res(ecoAsc)[1]
+  cellarea <- cellsize * cellsize / 1000000
+  bnd.area <- sum(table(values(ecoAsc))) * cellarea
+  
+  # get all non-NA values from reclassified ecoregion grid
+  ecoR <- raster(ecoRast)
+  ecoR[ecoR == 0] <- NA # convert zeros to NA
+  v.ecoR <- values(ecoR)
+  v.ecoR <- v.ecoR[!is.na(v.ecoR)]
+  
+  # convert raster values to data frame
+  nc <- data.frame(table(v.ecoR))
+  names(nc) <- c("class","bnd_km2")
+  nc$class <- as.numeric(levels(nc$class))
+  nc$bnd_km2[is.na(nc$bnd_km2)] <- 0
+  nc$bnd_km2 <- round(nc$bnd_km2 * cellarea, 3)
+  
+  # calculate ecoregion targets
+  nc$bnd_prop <- round(nc$bnd_km2/bnd.area,4) # bnd_prop
+  nc$target_km2 <- round(nc$bnd_prop*mdr,3) # target_km2 (MDR-based target)
+  nc <- nc[,c("class","target_km2","bnd_km2","bnd_prop")]
+  
+  # create summary table and write results
+  write.csv(nc,outFile,row.names=F,quote=T)
+  
+}
