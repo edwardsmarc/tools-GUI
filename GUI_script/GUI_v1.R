8aaa12366673b8b7c12e3f1abb1a8d2e5d079369
Marc Edwards <medwards219@gmail.com>
2017-02-09 18:10:53 -0800

    Added tab 4 - generates target tables

--- GUI_script/GUI_v1.R
+++ GUI_script/GUI_v1.R
@@ -15,24 +15,29 @@ rm(list=ls())
 GUI_v1 <- function(){
   
 #library(gWidgets)
-library(gWidgets2)
+#library(gWidgets2)
 #library(gWidgetstcltk) # using a different package here will access a different GUI toolkit.
 library(gWidgets2tcltk) # using a different package here will access a different GUI toolkit.
+#library(gWidgets2RGtk2)
+#library(gWidgetsRGtk2)
 source("../functions/createFolders.R")
+source("../functions/gen_ba_shp.r")
+source("../functions/gen_target_table.R")
 
   
 # set up high level containers here
-win <- gwindow("GUI_v1") # the full window container that will hold everything
+win <- gwindow("GUI_v1", toolkit = guiToolkit("tcltk")) # the full window container that will hold everything
 ##
 g <- ggroup(container = win, horizontal = FALSE, expand = TRUE) # a high level group that will hold everything
 nb <- gnotebook(container = g) # a notebook container
 ##
-nb1 <- ggroup(container = nb, label = "folders", horizontal = FALSE)
-#nb2 <- ggroup(container = nb, label = "denovoRep", horizontal = FALSE)
+nb1 <- ggroup(container = nb, label = "setup", horizontal = FALSE)
+nb2 <- ggroup(container = nb, label = "benchmark shp", horizontal = FALSE)
+nb4 <- ggroup(container = nb, label = "target tables", horizontal = FALSE)
 ##
 
 
-###### tab 1 - folder set up ############################################################
+###### tab 1 - folder set up and projection file ############################################################
 
 # browse to select parent directory where folders will be added
 gp_1_parent <- ggroup(container = nb1) 
@@ -84,10 +89,177 @@ gp_1_run_btn <- gbutton(
   }
 )
 
+gseparator(horizontal = TRUE, container = nb1, expand = TRUE)
+
+# browse to select builder csv file
+gp_1_proj <- ggroup(container = nb1) 
+gp_1_proj_lbl <- glabel("Shapefile projection file: ", container = gp_1_proj) # text label
+gp_1_proj_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "open", 
+  quote = TRUE, 
+  container = gp_1_proj, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Shapefile projection file: ", svalue(gp_1_proj_btn))
+  }
+)
+
+###### tab 2 - generate benchmark shapefiles ############################################################
+
+# browse to select builder csv file
+gp_2_buildCsv <- ggroup(container = nb2) 
+gp_2_buildCsv_lbl <- glabel("Builder csv file: ", container = gp_2_buildCsv) # text label
+gp_2_buildCsv_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "open", 
+  quote = TRUE, 
+  container = gp_2_buildCsv, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Builder csv file: ", svalue(gp_2_buildCsv_btn))
+  }
+)
+
+# browse to select catchments shp
+gp_2_catchShp <- ggroup(container = nb2) 
+gp_2_catchShp_lbl <- glabel("Catchments shapefile: ", container = gp_2_catchShp) # text label
+gp_2_catchShp_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "open", 
+  quote = TRUE, 
+  container = gp_2_catchShp, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Catchments shapefile: ", svalue(gp_2_catchShp_btn))
+  }
+)
+
+# browse to select output file
+gp_2_outFile <- ggroup(container = nb2) 
+gp_2_outFile_lbl <- glabel("Output shapefile: ", container = gp_2_outFile) # text label
+gp_2_outFile_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "save", 
+  quote = TRUE, 
+  container = gp_2_outFile, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Catchments shapefile: ", svalue(gp_2_outFile_btn))
+  }
+)
+
+# browse to select optional shapefiles file
+gp_2_addShp <- ggroup(container = nb2) 
+gp_2_addShp_lbl <- glabel("Additional shapefiles (optional): ", container = gp_2_addShp) # text label
+gp_2_addShp_btn <- gfilebrowse( # file browser
+  text = "",
+  type = "open",
+  quote = TRUE,
+  container = gp_2_addShp,
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Additional shapefiles: ", svalue(gp_2_addShp_btn))
+  }
+)
 
 
+# button to run the createFolders function
+gp_2_run <- ggroup(container = nb2)
+gp_2_run_btn <- gbutton(
+  text = "Generate benchmark shps",
+  container = gp_2_run,
+  handler = function(h, ...){ 
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Working...")
+    if(nchar(svalue(gp_1_proj_btn))==0){
+      svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "No projection file selected")
+      stop("No projection file selected")
+    }
+    tryCatch({
+      gen_ba_shp(
+        buildCsv = svalue(gp_2_buildCsv_btn), 
+        catchmentsShp = svalue(gp_2_catchShp_btn), 
+        outFile = svalue(gp_2_outFile_btn), 
+        prjFile = svalue(gp_1_proj_btn), 
+        shp.paths = svalue(gp_2_addShp_btn))
+      
+      svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Benchmark shapefiles created")
+    }, error = function(cond){
+      message("Could not generate shapefiles")
+      message(cond)
+    }
+    )
+  }
+)
 
-# output text box
+###### tab 4 - generate target tables ############################################################
+
+# browse to select ecoregion criteria raster
+gp_4_rastereco <- ggroup(container = nb4) 
+gp_4_rastereco_lbl <- glabel("Raster file: ", container = gp_4_rastereco) # text label
+gp_4_rastereco_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "open", 
+  quote = TRUE, 
+  container = gp_4_rastereco, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Raster criteria file: ", svalue(gp_4_rastereco_btn))
+  }
+)
+
+gp_4_rasterbnd <- ggroup(container = nb4) 
+gp_4_rasterbnd_lbl <- glabel("Raster file: ", container = gp_4_rasterbnd) # text label
+gp_4_rasterbnd_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "open", 
+  quote = TRUE, 
+  container = gp_4_rasterbnd, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Raster boundary file: ", svalue(gp_4_rasterbnd_btn))
+  }
+)
+
+gp_4_mdr <- ggroup(container = nb4)
+gp_4_mdr_lbl <- glabel("MDR value: ", container = gp_4_mdr)
+gp_4_mdr_txt <- gedit(text = "", 
+                      container = gp_4_mdr,
+                      handler = function(h,...){
+                        svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "MDR: ", svalue(gp_4_mdr_txt))
+                      })
+
+gp_4_outFile <- ggroup(container = nb4) 
+gp_4_outFile_lbl <- glabel("Output shapefile: ", container = gp_4_outFile) # text label
+gp_4_outFile_btn <- gfilebrowse( # file browser
+  text = "", 
+  type = "save", 
+  quote = TRUE, 
+  container = gp_4_outFile, 
+  handler = function(h,...){
+    svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Catchments shapefile: ", svalue(gp_4_outFile_btn))
+  }
+)
+
+# button to run the createFolders function
+gp_4_run <- ggroup(container = nb4)
+gp_4_run_btn <- gbutton(
+  text = "Generate target table",
+  container = gp_4_run,
+  handler = function(h, ...){ 
+    tryCatch({
+      gen_target_table(
+        mdrVal = svalue(gp_4_mdr_txt), 
+        ecoRast = svalue(gp_4_rastereco_btn), 
+        ecoBnd = svalue(gp_4_rasterbnd_btn), 
+        outFile = svalue(gp_4_outFile_btn) 
+        )
+      
+      svalue(txtOutput) <- paste0(svalue(txtOutput), "\n", "Target tables created")
+    }, error = function(cond){
+      message("Could not generate target tables")
+      message(cond)
+    }
+    )
+  }
+)
+
+
+#### output text box ############################################################
+
 frmOutput <- gframe("Output", container = g)
 txtOutput <- gtext("", container = frmOutput)
 
