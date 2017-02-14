f08a9302845a497f1e87dc257fbdc012c209d79e
Marc Edwards <medwards219@gmail.com>
2017-02-14 08:53:09 -0800

    Function to generate mdr based target tables. Based on NWB function but simplified to just output targets. No pa values and no columns describing class values.

--- functions/gen_target_table.R
+++ functions/gen_target_table.R
@@ -3,28 +3,43 @@
 # New function based on evalTargets_NWB.R to generate target tables based on MDR representation. Proportion of each class in the ecoregion is multiplied by the MDR value to get target.
 # based on evalTargets_NWB, simplified by removing protected area values from tables. Also removes range and type columns that showed raster value descriptors. Users should now refer to their raster value class breaks in a separate table.
   
-gen_target_table <- function(mdr, ecoRast, ecoBnd, outFile) {
+gen_target_table <- function(mdrVal, ecoRast, ecoBnd, outFile) {
   
   library(raster)
 
   # Checks 
   ############################
-  # raster values exist
+  # ecoRast exists
   if(file.exists(ecoRast)){
     ecoR <- raster(ecoRast)
   } else{
     stop(paste0("File does not exist: ", ecoRast))
   }
   
+  # ecoBnd exists
+  if(file.exists(ecoBnd)){
+    bndR <- raster(ecoBnd)
+  } else{
+    stop(paste0("File does not exist: ", ecoBnd))
+  }
+  
+  # ecoRast and ecoBnd have same cell size
+  if(!res(ecoR)[1] == res(bndR)[1]){
+    stop("Raster files have different cell sizes!")
+  }
+  
+  # MDR value provided - make sure numeric
+  if(nchar(mdrVal) > 0){
+    mdr <- as.numeric(mdrVal)
+  }
+  
   # Calculate ecoregion area using eco boundary
-  ecoAsc <- raster(ecoBnd)
-  ecoAsc[ecoAsc == 0] <- NA # If raster has zero values filling the extent, convert them to NA 
-  cellsize <- res(ecoAsc)[1]
+  bndR[bndR == 0] <- NA # If raster has zero values filling the extent, convert them to NA 
+  cellsize <- res(bndR)[1]
   cellarea <- cellsize * cellsize / 1000000
-  bnd.area <- sum(table(values(ecoAsc))) * cellarea
+  bnd.area <- sum(table(values(bndR))) * cellarea
   
   # get all non-NA values from reclassified ecoregion grid
-  ecoR <- raster(ecoRast)
   ecoR[ecoR == 0] <- NA # convert zeros to NA
   v.ecoR <- values(ecoR)
   v.ecoR <- v.ecoR[!is.na(v.ecoR)]
